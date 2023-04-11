ab1ab3f7332d40dcd49b8bc3f8748fd29f0d14b0
Randy1005 <randychang.10@gmail.com>
2023-04-11 17:30:10 -0600

    added vertex/edge structs

--- ink/ink.hpp
+++ ink/ink.hpp
@@ -1,6 +1,28 @@
 #include <ot/timer/timer.hpp>
+#include <string>
 
 namespace ink {
+
+
+struct Vert {
+
+};
+
+struct Edge {
+
+};
+
+
+
+
+class Ink {
+public:
+	void read_graph(const std::string& name);
+
+private:
+
+
+};
 
 
 
